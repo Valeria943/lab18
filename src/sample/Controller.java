b098e491d502cd306d2d64a77be6360d6b3af537
Valeria943 <82813302+Valeria943@users.noreply.github.com>
2022-05-24 16:10:54 +0900

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class Controller {
+    @FXML
+    private TextArea tarea;
+
+    @FXML
+    private TextField tfield;
+
+    @FXML
+    private void clickbut(ActionEvent event){
+        tarea.appendText("Your message: " + tfield.getText() + "\n");
+        tfield.setText(" ");
+        tfield.requestFocus();
+    }
+
+    @FXML
+    private void clickenter(KeyEvent eventTwo) {
+        if (eventTwo.getCode() == KeyCode.ENTER) {
+            tarea.appendText("Your message: " + tfield.getText() + "\n");
+            tfield.setText(" ");
+            tfield.requestFocus();
+        }
+    }
+}
